beef9c267497c9e9250f1eb0e5fd8f1fa3a96772
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:42:01 +0100

    Create LansirnaRampa1.java

---
+++ src/lv7/drugi/LansirnaRampa1.java
@@ -0,0 +1,13 @@
+class LansirnaRampa1 {
+    synchronized void lansiraj() {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Odbrojavanje: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Lansiranje!");
+    }
+}

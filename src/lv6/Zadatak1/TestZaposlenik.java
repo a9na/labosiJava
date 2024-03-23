b9ebf590566d0724a333985056f2d4c0df332579
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:36:42 +0100

    Create TestZaposlenik.java

---
+++ src/lv6/Zadatak1/TestZaposlenik.java
@@ -0,0 +1,21 @@
+package Zadatak1;
+
+import Zadatak1.Direktor;
+import Zadatak1.Tajnica;
+
+public class TestZaposlenik {
+    public static void main(String[] args) {
+        Direktor direktor = new Direktor("D123", "Ivan", "Ivanovic");
+        Tajnica tajnica = new Tajnica("T456", "Ana", "Anic");
+        Vozac vozac = new Vozac("V789", "Marko", "Markovic");
+
+        direktor.predstaviSe();
+        direktor.radi();
+
+        tajnica.predstaviSe();
+        tajnica.radi();
+
+        vozac.predstaviSe();
+        vozac.radi();
+    }
+}

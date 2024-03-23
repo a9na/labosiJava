d31386fd9bd4a4081e1dd2060d588692d3d8a36c
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:36:18 +0100

    Create Tajnica.java

---
+++ src/lv6/Zadatak1/Tajnica.java
@@ -0,0 +1,12 @@
+package Zadatak1;
+
+public class Tajnica extends Zaposlenik {
+    public Tajnica(String ID, String ime, String prezime) {
+        super(ID, ime, prezime);
+    }
+
+    @Override
+    public void radi() {
+        System.out.println("Dogovaram");
+    }
+}

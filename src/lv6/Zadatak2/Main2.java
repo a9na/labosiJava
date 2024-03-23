6d81cb60925d67573410be4976a489f35755e9c1
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:38:05 +0100

    Create Main2.java

---
+++ src/lv6/Zadatak2/Main2.java
@@ -0,0 +1,17 @@
+package Zadatak2;
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        Zivotinja konj = new Konj();
+        Zivotinja riba = new Riba();
+        Zivotinja ptica = new Ptica();
+
+        konj.kreciSe();
+        riba.kreciSe();
+        ptica.kreciSe();
+
+        System.out.println("-------------------------");
+    }
+
+}

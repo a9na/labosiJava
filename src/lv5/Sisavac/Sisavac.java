5ebc13f55c692101dd9dd21b9b89f265c62ee913
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:27:23 +0100

    Create Sisavac.java

---
+++ src/lv5/Sisavac/Sisavac.java
@@ -0,0 +1,27 @@
+package Sisavac;
+
+public class Sisavac {
+    protected int dob;
+    protected double tezina;
+
+    public Sisavac() {
+        this.dob = 0;
+        this.tezina = 0.0;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+}

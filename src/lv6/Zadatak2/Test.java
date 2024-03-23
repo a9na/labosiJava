5a82df3bb5e6ee903712c51c89eeb9827fb8023c
Ana Novković <anaiscoding@gmail.com>
2024-03-23 16:39:10 +0100

    Create Test.java

---
+++ src/lv6/Zadatak2/Test.java
@@ -0,0 +1,13 @@
+package Zadatak2;
+
+public class Test {
+    public static void main(String[] args) {
+        Zivotinja konj = new Konj();
+        Zivotinja riba = new Riba();
+        Zivotinja ptica = new Ptica();
+
+        konj.kreciSe();
+        riba.kreciSe();
+        ptica.kreciSe();
+    }
+}
